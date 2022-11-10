7f55b01f8c476fdc8ca722537f6f94cce560de8e
Wae Waee <waewaee@gmail.com>
2022-11-10 20:14:11 +0630

    - add custom attribute to RoundedImageView

--- app/src/main/java/com/waewaee/ww_custom_view_components/veiws/components/RoundedCornerImageView.kt
+++ app/src/main/java/com/waewaee/ww_custom_view_components/veiws/components/RoundedCornerImageView.kt
@@ -6,20 +6,29 @@ import android.graphics.Path
 import android.graphics.RectF
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.content.withStyledAttributes
+import com.waewaee.ww_custom_view_components.R
 
 class RoundedCornerImageView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : AppCompatImageView(context, attrs, defStyleAttr) {
 
-    private var cornerRadius = 32f
+    private var cornerRadius = 0f
 
     private val path = Path()
 
+    init {
+
+        context.withStyledAttributes(attrs, R.styleable.RoundedCornerImageView) {
+            cornerRadius = getDimension(R.styleable.RoundedCornerImageView_cornerRadius, 0f)
+        }
+    }
+
     override fun onDraw(canvas: Canvas?) {
 
-        val rectangle = RectF(0f,0f,width.toFloat(),height.toFloat())
+        val rectangle = RectF(0f, 0f, width.toFloat(), height.toFloat())
 
-        path.addRoundRect(rectangle,cornerRadius,cornerRadius,Path.Direction.CCW)
+        path.addRoundRect(rectangle, cornerRadius, cornerRadius, Path.Direction.CCW)
 
         canvas?.clipPath(path)
 
