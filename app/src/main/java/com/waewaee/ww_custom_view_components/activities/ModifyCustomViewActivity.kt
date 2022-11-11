613cab173aa33cb12ad4cbe81c80e2e6a82d2017
Wae Waee <waewaee@gmail.com>
2022-11-11 18:17:38 +0630

    - State Progress View ( Composite Custom View)

---
+++ app/src/main/java/com/waewaee/ww_custom_view_components/activities/ModifyCustomViewActivity.kt
@@ -0,0 +1,58 @@
+package com.waewaee.ww_custom_view_components.activities
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.waewaee.ww_custom_view_components.R
+import com.waewaee.ww_custom_view_components.veiws.viewpods.StateProgressViewPod
+import kotlinx.android.synthetic.main.activity_modify_custom_view.*
+
+class ModifyCustomViewActivity : AppCompatActivity() {
+
+    private lateinit var mViewPodStateProgress: StateProgressViewPod
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modify_custom_view)
+
+        setUpViewPod()
+
+        setListener()
+    }
+
+    private fun setUpViewPod() {
+        mViewPodStateProgress = viewPodStateProgress as StateProgressViewPod
+        mViewPodStateProgress.apply {
+            setStepDescription("Survey", "Cabling", "Splicing", "Activate")
+        }
+    }
+
+    private fun setListener() {
+
+        btn1.setOnClickListener {
+            mViewPodStateProgress.onTapStep1()
+        }
+
+        btn2.setOnClickListener {
+            mViewPodStateProgress.onTapStep2()
+        }
+
+        btn3.setOnClickListener {
+            mViewPodStateProgress.onTapStep3()
+        }
+
+        btn4.setOnClickListener {
+            mViewPodStateProgress.onTapStep4()
+            mViewPodStateProgress.completeAllSteps()
+        }
+
+    }
+
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, ModifyCustomViewActivity::class.java)
+        }
+    }
+}
