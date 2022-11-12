7ff7c7d73f13c8f910803688d9399df3aa4a929d
Wae Waee <waewaee@gmail.com>
2022-11-12 16:38:53 +0630

    - handle user's action

--- app/src/main/java/com/waewaee/ww_custom_view_components/activities/CustomComponentActivity.kt
+++ app/src/main/java/com/waewaee/ww_custom_view_components/activities/CustomComponentActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.waewaee.ww_custom_view_components.R
+import com.waewaee.ww_custom_view_components.veiws.components.EmotionalFaceView
+import kotlinx.android.synthetic.main.activity_custom_component.*
 
 class CustomComponentActivity : AppCompatActivity() {
 
@@ -12,6 +14,15 @@ class CustomComponentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_custom_component)
 
+        // 1
+        happyButton.setOnClickListener {
+            emotionalFaceView.happinessState = EmotionalFaceView.HAPPY
+        }
+
+        // 2
+        sadButton.setOnClickListener {
+            emotionalFaceView.happinessState = EmotionalFaceView.SAD
+        }
     }
 
     companion object {
